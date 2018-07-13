66e5a69f82aa039a85bcf36c9ee0bdc33176b83d
yanstolyarov <yanstolyarov@mail.ru>
2018-07-13 15:05:02 +0300

    button test3

--- button2.py
+++ button2.py
@@ -18,7 +18,7 @@ while True:
         #take a reading
         input = GPIO.input(17)
         #if the last reading was low and this one high, print
-        if ((not prev_input) and input):
+        if ((prev_input) and input):
             print("Button pressed")
             #update previous input
             prev_input = input
