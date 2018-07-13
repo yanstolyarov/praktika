57f6eff76eaddcbdf019f69d553235e6fb4987e0
yanstolyarov <yanstolyarov@mail.ru>
2018-07-13 14:09:17 +0300

    button test

--- test.py
+++ test.py
@@ -3,6 +3,10 @@
 from RPi import GPIO
 from gpiozero import DistanceSensor
 import time
+from gpiozero import Button
+from signal import pause
+
+button = Button(2)
 
 sensor1 = DistanceSensor(23, 24)
 sensor2 = DistanceSensor(27, 22)
@@ -29,6 +33,9 @@ p_w1 = GPIO.PWM(e1,100)
 p_w2 = GPIO.PWM(e2,100)
 p_w1.start(0)
 p_w2.start(0)
+
+enab1 = GPIO.output(21, GPIO.HIGH)
+enab2 = GPIO.output(19, GPIO.HIGH)
 
 def motor_pwm_forw_1(x):
     p_w1.ChangeDutyCycle(x)
@@ -58,56 +65,18 @@ def all_motor_pwm_reverse(x):
     p_w2.ChangeDutyCycle(x)
     GPIO.output(m2, GPIO.HIGH)
 
-while True:
-    print('front_distance', 0.001*sensor1.distance, 'mm')
-    print('left_distance', 0.001*sensor2.distance, 'mm')
-    current_time = time.time()
-    if button.is_pressed:
-        print("button_is_pressed")
-        time.sleep(2)
-        #turn off the engines
-        all_motor_pwm_forward(0)
-        if sensor2.distance < const_l2:
-            #then, on the left, the impasse must be turned to the right
-            t = time.time()
-            #reverse
-            all_motor_pwm_reverse(90)
-            time.sleep(2)
-            #stop
-            all_motor_pwm_forward(0)
-            #power to the left wheel
-            motor_pwm_forw_1(90)
-            time.sleep(turn)
-            #turn completed
-            #power supply for both wheels
-            all_motor_pwm_forward(90)
-            time.sleep(2)
-        else:
-            #need a left turn
-            #reverse
-            all_motor_pwm_reverse(90)
-            time.sleep(2)
-            #power to the right wheel
-            motor_pwm_forw_2(90)
-            time.sleep(turn)
-            #turn completed
-            #power supply for both wheels
-            all_motor_pwm_forward(90)
-            time.sleep(2)
+def info():
+    print('sensor1',sensor1.distance,'m')
+    print('sensor2',sensor2.distance,'m')
 
-    else:
-        print("Button is not pressed")
-        #power supply for both wheels
-        all_motor_pwm_forward(90)
-        if sensor2.distance < const_l1:
-            #stop right motor
-            motor_pwm_forw_2(0)
-            sleep(0.001)
-            #start right motor
-            motor_pwm_forw_2(90)
-        if sensor2.distance > const_l1:
-            #stop left motor
-            motor_pwm_forw_1(0)
-            sleep(0.001)
-            #start left motor
-            motor_pwm_forw_1(90)
+for t in range(20):
+    t += 1
+    info()
+    motor_pwm_forw_1(30)
+    motor_pwm_reverse_2(50)
+    time.sleep(2)
+    info()
+    all_motor_pwm_forward(90)
+    time.sleep(2)
+    all_motor_pwm_reverse(40)
+    info()
