39af93601e4b5b4010a2c97c2fc96c7f74f31dc4
yanstolyarov <yanstolyarov@mail.ru>
2018-07-16 15:48:58 +0300

    test

--- distance_sensor_script.py
+++ distance_sensor_script.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python
-#from gpiozero import DistanceSensor
-#from time import sleep
-
-#sensor1 = DistanceSensor(20, 16)#echo first then trick
-#sensor2 = DistanceSensor(22, 27)
-
-
-#def dist1():
-#    global dist1
-#    dist1 = sensor1.distance
-#    print('dist1:', dist1)
-
-#def dist2():
-#    global dist2
-#    dist1 = sensor2.distance
-#    print('dist2:', dist2)
-
-#while True:
-#    dist1 = sensor1.distance
-#    print('dist1:', dist1)
-    #dist1()
-    #dist2()
-
-#---example---
-
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(5, 11)
+sensor1 = DistanceSensor(23, 24)#echo first then trick
+sensor2 = DistanceSensor(5, 11)
+
+
+def dist1():
+    global dist1
+    dist1 = sensor1.distance
+    print('dist1:', dist1)
+
+def dist2():
+    global dist2
+    dist1 = sensor2.distance
+    print('dist2:', dist2)
 
 while True:
-    print('Distance to nearest object is', sensor.distance, 'm')
-    sleep(1)
+#    dist1 = sensor1.distance
+#    print('dist1:', dist1)
+    dist1()
+    dist2()
+
+
+
+
+#---example---
+#
+#from gpiozero import DistanceSensor
+#from time import sleep
+#
+#sensor = DistanceSensor(5, 11)#echo and trigger
+#
+#while True:
+#    print('Distance to nearest object is', sensor.distance, 'm')
+#    sleep(1)
