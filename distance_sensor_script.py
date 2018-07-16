8c8fdb852595f9faf47b194bb4be07524af6ffa1
yanstolyarov <yanstolyarov@mail.ru>
2018-07-16 16:21:13 +0300

    test

--- distance_sensor_script.py
+++ distance_sensor_script.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-from gpiozero import DistanceSensor
-from time import sleep
+#from gpiozero import DistanceSensor
+#from time import sleep
 
-sensor1 = DistanceSensor(18, 23)#echo first then trick
+#sensor1 = DistanceSensor(23, 24)#echo first then trick
 #sensor2 = DistanceSensor(5, 11)
 
 
-def dist1():
-    print('dist1:', sensor1.distance)
+#def dist1():
+#    print('dist1:', sensor1.distance)
 
 #def dist2():
 #    print('dist2:', sensor2.distance)
 
-while True:
-    dist1()
+#while True:
+#    dist1()
 #    dist2()
 
 
@@ -21,11 +21,11 @@ while True:
 
 #---example---
 #
-#from gpiozero import DistanceSensor
-#from time import sleep
-#
-#sensor = DistanceSensor(5, 11)#echo and trigger
-#
-#while True:
-#    print('Distance to nearest object is', sensor.distance, 'm')
-#    sleep(1)
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor = DistanceSensor(23, 24)#echo and trigger
+
+while True:
+    print('Distance to nearest object is', sensor.distance, 'm')
+    sleep(1)
