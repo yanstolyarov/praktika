b856877f1a8b4d817e7714bbf25272787c8eec78
yanstolyarov <yanstolyarov@mail.ru>
2018-07-17 15:35:26 +0300

    line sensor

--- com_line_sensor.py
+++ com_line_sensor.py
@@ -6,10 +6,12 @@ channel_1 = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel_1, GPIO.IN)
-
+print('test')
 NUM_CYCLES = 10
 while True:
+    print('test1')
     start = time.time()
+    print(start)
     for impulse_count in range(NUM_CYCLES):
         GPIO.wait_for_edge(channel_1, GPIO.FALLING)
     duration = time.time() - start      #seconds to run for loop
