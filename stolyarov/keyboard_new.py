abc5e85fe347e7e7f0c793597a5d61e52e084dc7
yanstolyarov <yanstolyarov@mail.ru>
2018-07-19 14:09:11 +0300

    stv mod1

--- stolyarov/keyboard_new.py
+++ stolyarov/keyboard_new.py
@@ -4,8 +4,10 @@
 
 import sys, termios, tty, os, time
 import motor_driver_new as MD
-from gpiozero import DistanceSensor
 from time import sleep
+import com_distance_sensor_1 as DS1
+import com_distance_sensor_2 as DS2
+import button_script as BS
 
 from RPi import GPIO
 #initialise a previous input variable to 0 (assume button not pressed last)
@@ -25,27 +27,6 @@ prev2 = 1
 curr1 = 1
 curr2 = 2
 
-sensor1 = DistanceSensor(20, 16)#echo first then trick
-sensor2 = DistanceSensor(22, 27)
-
-def button1_status(pin):
-    global curr1
-    global prev1
-    curr1 = GPIO.input(pin)
-    #print("b1",curr1)
-    if ((prev1 == 0) and curr1 == 0):
-        print("Button1 pressed")
-    prev1 = curr1
-
-def button2_status(pin):
-    global curr2
-    global prev2
-    curr2 = GPIO.input(pin)
-    #print("b2",curr2)
-    if ((prev2 == 0) and curr2 == 0):
-        print("Button2 pressed")
-    prev2 = curr2
-
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -57,21 +38,12 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def dist1():
-    global dist1
-    dist1 = sensor1.distance
-    print('dist1:', dist1)
-
-def dist2():
-    global dist2
-    dist1 = sensor2.distance
-    print('dist2:', dist2)
 
 while True:
-    dist1()
-    dist2()
-    button1_status(17)
-    button2_status(10)
+    print('button1: ',BS.button1_status())
+    print('button2: ',BS.button2_status())
+    print('dm1: ',DS1.dm1())
+    print('dm2: ',DS2.dm2())
 
     char = getch()
 
@@ -80,27 +52,27 @@ while True:
         print("Stop!")
         exit(0)
 
-    elif (char == "d"):
+    elif (char == "a"):
         print("Left pressed")
         MD.motor_pwm_forw_2(power)
         MD.motor_pwm_reverse_1(power)
         time.sleep(button_delay)
         MD.stop()
 
-    elif (char == "a"):
+    elif (char == "d"):
         print("Right pressed")
         MD.motor_pwm_forw_1(power)
         MD.motor_pwm_reverse_2(power)
         time.sleep(button_delay)
         MD.stop()
 
-    elif (char == "s"):
+    elif (char == "w"):
         print("Up pressed")
         MD.all_motor_pwm_forward(power)
         time.sleep(button_delay)
         MD.stop()
 
-    elif (char == "w"):
+    elif (char == "s"):
         print("Down pressed")
         MD.all_motor_pwm_reverse(power)
         time.sleep(button_delay)
