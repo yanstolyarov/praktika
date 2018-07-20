2dbee35d79378a6a521bfebf6bb922914309f3d6
IlyaNikitenko <40904986+IlyaNikitenko@users.noreply.github.com>
2018-07-20 14:40:00 +0300

    Update keyboard_new.py
    Believe that it's the last version

--- Nikitenko/keyboard_new.py
+++ Nikitenko/keyboard_new.py
@@ -4,8 +4,7 @@
 
 import sys, termios, tty, os, time
 import motor_driver_new as MD
-import com_distance_sensor1 as DS1
-import com_distance_sensor2 as DS2
+from time import sleep
 from RPi import GPIO
 #initialise a previous input variable to 0 (assume button not pressed last)
 #prev_input = 0
@@ -14,33 +13,36 @@ from RPi import GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.IN)
 GPIO.setup(10,GPIO.IN)
-
+p1= 30 #on the left engine to offset
+p2=40  #on the right engine to offset
+p3 = 90 #way back for both engine 
+p4=70 #on the left engine to turn left
+p5=90 #on the right engine to turn left
+p6=50 #way ahead for both engine
+p7=70 #on the left engine for turn right
+p8=60 #on the left engine for turn right
+but_1 = BS.button1_status()
+but_2 = BS.button2_status()
 sleep = 0.1
 power = 40
 button_delay = 0.3
 
-#side = right
-t_a = 0.2 #time of moving forward/backward to make 1 step
-t_b = 0.3 #time for turn on 90 degrees
-p_s = 90 #power for steps
-p_t = 60 #power for turns
-c_a = 25 #dalnomer critical value
 
 def button1_status():
     curr1 = GPIO.input(17)
     #print("b1",curr1)
     if curr1 == 0:
-        return "True"
+        return 1
     else:
-        return "False"
+        return 0
 
 def button2_status():
     curr2 = GPIO.input(10)
     #print("b2",curr2)
     if curr2 == 0:
-        return "True"
+        return 1
     else:
-        return "False"
+        return 0
 
 def getch():
     fd = sys.stdin.fileno()
@@ -55,54 +57,39 @@ def getch():
 
 
 while True:
-    but_f = button1_status()
-    dm_2 = DS2.dm_2()
-    print('button1: ',button1_status())
-    print('button2: ',button2_status())
-    print('dm1: ', DS1.dm_1())
-    print('dm2: ', DS2.dm_2())
-    time.sleep(0.01)
-
-
-    if but_f == "False" and DS.dm_2() <= c_a:
-        MD.all_motor_pwm_forward(p_s)
-        sleep(t_a)
+    but_1 = BS.button1_status()
+    but_2 = BS.button2_status()
+    if but_1==0 and but_2==0
+        while but_2 != 1
+            but_2 = BS.button2_status()
+            MD.motor_pwm_forw_1(p1)
+            MD.motor_pwm_forw_2(p2)
+            sleep(1)
+            MD.stop()
+    if but_1==0 and but_2==1
+        while but_1 != 1
+            but_1 = BS.button1_status()
+            MD.motor_pwm_forw_1(p6+5)
+            MD.motor_pwm_forw_2(p6)
+            sleep(1)
+            MD.stop()
+    if but_1==1 and but_2=0
+        MD.motor_pwm_reverse_1(p3)
+        MD.motor_pwm_reverse_2(p3)
+        sleep(1)
         MD.stop()
-
-    if but_f == "False" and DS.dm_2() > c_a:
-        MD.all_motor_pwm_forward(p_s)
-        sleep(t_a)
+        MD.motor_pwm_forw_1(p4)
+        MD.motor_pwm_forw_2(p5)
+        sleep(1)
         MD.stop()
-        MD.motor_pwm_forw_1(p_t)
-        MD.motor_pwm_reverse_2(p_t)
-        sleep(t_b)
-        MD.stop()
-        MD.all_motor_pwm_forward(p_s)
-        sleep(t_a)
-        MD.stop()
-
-    if but_f == "True" and DS.dm_2() <= c_a:
-        MD.all_motor_pwm_reverse(p_s)
-        sleep(t_a)
-        MD.stop()
-        MD.motor_pwm_forw_2(p_t)
-        MD.motor_pwm_reverse_1(p_t)
-        sleep(t_b)
-        MD.stop()
-        MD.all_motor_pwm_forward(p_s)
-        sleep(t_a)
-        MD.stop()
-
-    if but_f == "True" and DS.dm_2() <= c_a:
-        MD.all_motor_pwm_reverse(p_s)
-        sleep(t_a)
-        MD.stop()
-        MD.motor_pwm_forw_1(p_t)
-        MD.motor_pwm_reverse_2(p_t)
-        sleep(t_b)
-        MD.stop()
-        MD.all_motor_pwm_forward(p_s)
-        sleep(t_a)
+    if but_1==1 and but_2=1
+        MD.motor_pwm_reverse_1(p3)
+        MD.motor_pwm_reverse_2(p3)
+        sleep(1)
+        MD.stop()         
+        MD.motor_pwm_forw_1(p7)
+        MD.motor_pwm_forw_2(p8)
+        sleep(1)
         MD.stop()
 
 
@@ -117,31 +104,31 @@ while True:
         print("Left pressed")
         MD.motor_pwm_forw_2(power)
         MD.motor_pwm_reverse_1(power)
-        time.sleep(button_delay)
+        sleep(button_delay)
         MD.stop()
 
     elif (char == "d"):
         print("Right pressed")
         MD.motor_pwm_forw_1(power)
         MD.motor_pwm_reverse_2(power)
-        time.sleep(button_delay)
+        sleep(button_delay)
         MD.stop()
 
     elif (char == "w"):
         print("Up pressed")
         MD.all_motor_pwm_forward(power)
-        time.sleep(button_delay)
+        sleep(button_delay)
         MD.stop()
 
     elif (char == "s"):
         print("Down pressed")
         MD.all_motor_pwm_reverse(power)
-        time.sleep(button_delay)
+        sleep(button_delay)
         MD.stop()
 
     elif (char == "1"):
         print("Number 1 pressed")
-        time.sleep(button_delay)
+        sleep(button_delay)
     elif (char == " "):
         print('spacebar')
     else:
